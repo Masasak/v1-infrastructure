691b0c43567a91abb6bce859423eb70a1208880d
Junho-06 <k34920032@gmail.com>
2023-11-27 01:41:02 +0900

    add :: root local variable file

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  region = "ap-northeast-2"
+  
+  name_prefix     = "SnapVibe"
+  azs             = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}b"]
+  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
+  private_subnets = ["10.0.128.0/20", "10.0.144.0/20"]
+  vpc_cidr        = "10.0.0.0/16"
+}
--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,3 @@
-locals {
-  name_prefix     = "SnapVibe"
-  azs             = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}b"]
-  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
-  private_subnets = ["10.0.128.0/20", "10.0.144.0/20"]
-  vpc_cidr        = "10.0.0.0/16"
-}
-
 module "vpc" {
   source = "./modules/vpc"
 
