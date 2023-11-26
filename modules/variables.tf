d1f36275e021d7e32947361f6367629162419dfb
Junho-06 <k34920032@gmail.com>
2023-11-26 22:40:51 +0900

    add :: vpc module

---
+++ modules/variables.tf
@@ -0,0 +1,24 @@
+variable "name_prefix" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type    = list(string)
+}
+
+variable "public_subnets" {
+  type    = list(string)
+}
+
+variable "private_subnets" {
+  type    = list(string)
+}
+
+variable "public_subnet_tags_per_az" {
+  type        = map(map(string))
+  default     = {}
+}
---
+++ modules/vpc/main.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.1"
+
+  name = "${var.name_prefix}-vpc"
+  cidr = var.vpc_cidr
+  azs  = var.azs
+
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  public_subnet_tags_per_az = var.public_subnet_tags_per_az
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  nat_gateway_tags = {
+    Name = "${var.name_prefix}-nat"
+  }
+
+  igw_tags = {
+    Name = "${var.name_prefix}-igw"
+  }
+  map_public_ip_on_launch = true
+}
