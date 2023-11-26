40ed9716c94dd66d948df1c0200557d3678f6976
Junho-06 <k34920032@gmail.com>
2023-11-26 22:54:18 +0900

    add :: vpc impl

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+locals {
+  name_prefix     = "SnapVibe"
+  azs             = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}b"]
+  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
+  private_subnets = ["10.0.128.0/20", "10.0.144.0/20"]
+  vpc_cidr        = "10.0.0.0/16"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr        = local.vpc_cidr
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  name_prefix     = local.name_prefix
+}
