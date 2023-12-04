4f8af78c60533ef6edebea1fa140bf320d55e667
Junho-06 <k34920032@gmail.com>
2023-12-04 09:13:13 +0900

    delete :: eks module kubernetes provider

--- modules/eks/provider.tf
+++ modules/eks/provider.tf
@@ -5,21 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.23"
     }
-    kubernetes = {
-      source  = "hasicorp/kubernetes"
-      version = "~> 2.21"
-    }
   }
 }
 
 provider "aws" {
   region = "ap-northeast-2"
   alias  = "seoul"
-}
-
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
 }
