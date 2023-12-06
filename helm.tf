8ef4269f9321c56898d044388aa152bdf8cbbfda
Junho-06 <k34920032@gmail.com>
2023-12-06 15:22:29 +0900

    add :: helm impl provider

---
+++ helm.tf
@@ -0,0 +1,7 @@
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = module.eks.cluster_ca_certificate
+    token                  = module.eks.cluster_auth_token
+  } 
+}
