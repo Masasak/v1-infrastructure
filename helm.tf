b8fcb46abc0fe3234e23463142b8b2235cfe8c92
Junho-06 <k34920032@gmail.com>
2023-12-07 01:01:25 +0900

    add :: aws load balancer controller helm

--- helm.tf
+++ helm.tf
@@ -12,6 +12,10 @@ locals {
   argocd-name      = "argocd"
   argocd-version   = "0.1.0"
   argocd-namespace = "argocd"
+
+  aws-loadbalancer-controller-name      = "aws-load-balancer-controller"
+  aws-loadbalancer-controller-version   = "0.1.0"
+  aws-loadbalancer-controller-namespace = "kube-system"
 }
 
 module "argocd" {
@@ -21,4 +25,13 @@ module "argocd" {
   repository    = local.snapvibe-helm-repository
   chart         = local.argocd-name
   chart_version = local.argocd-version
+}
+
+module "aws-loadbalancer-controller" {
+  source        = "./modules/helm"
+  name          = local.aws-loadbalancer-controller-name
+  namespace     = local.aws-loadbalancer-controller-namespace
+  repository    = local.snapvibe-helm-repository
+  chart         = local.aws-loadbalancer-controller-name
+  chart_version = local.aws-loadbalancer-controller-version
 }
