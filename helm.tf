25074d45daf6d4f8795ef740113f5c5a3fd7d73b
Junho-06 <k34920032@gmail.com>
2023-12-07 17:01:01 +0900

    update :: vpc ipv6 disable

--- helm.tf
+++ helm.tf
@@ -18,7 +18,7 @@ locals {
   aws-load-balancer-controller-namespace = "kube-system"
 
   ingress-name      = "snapvibe-ingress"
-  ingress-version   = "0.1.3"
+  ingress-version   = "0.1.4"
   ingress-namespace = "default"
 }
 
