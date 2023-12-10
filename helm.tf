6284977b9ab68fbfc4052199b4ea4ba8c7a0ec36
Junho-06 <k34920032@gmail.com>
2023-12-11 00:57:20 +0900

    update :: helm chart version up

--- helm.tf
+++ helm.tf
@@ -22,7 +22,7 @@ locals {
   snapvibe-application-namespace = "argocd"
 
   ingress-name      = "ingress"
-  ingress-version   = "0.1.0"
+  ingress-version   = "0.1.1"
   ingress-namespace = "argocd"
 }
 
