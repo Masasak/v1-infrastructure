b195480f22cd859a93e6e46c224b4f77319dbc48
Junho-06 <k34920032@gmail.com>
2023-12-26 14:08:01 +0900

    add :: eks module data depends_on

--- modules/eks/data.tf
+++ modules/eks/data.tf
@@ -2,8 +2,10 @@ data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [module.eks.cluster_name]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [module.eks.cluster_name]
 }
